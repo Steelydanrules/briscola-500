935c642a1b7d90a9fde1d667c5d4ab200ee24093
steelydanrules <karl.konetsky@gmail.com>
2020-06-21 23:13:36 -0400

    setup.  it wont ignore node modules

---
+++ src/deck.js
@@ -0,0 +1,51 @@
+export const DENARI = "DENARI" //Gold
+export const SPADE = "SPADE" //Sword
+export const COPPE = "COPPE" //Cups
+export const BASTONI = "BASTONI" //Bats
+
+export const Deck = {
+  "2DENARI" : { suit: DENARI, points: 0, rank: 1},
+  "4DENARI" : { suit: DENARI, points: 0, rank: 2},
+  "5DENARI" : { suit: DENARI, points: 0, rank: 3},
+  "6DENARI" : { suit: DENARI, points: 0, rank: 4},
+  "7DENARI" : { suit: DENARI, points: 0, rank: 5},
+  "JDENARI" : { suit: DENARI, points: 2, rank: 6},
+  "QDENARI" : { suit: DENARI, points: 3, rank: 7},
+  "KDENARI" : { suit: DENARI, points: 4, rank: 8},
+  "3DENARI" : { suit: DENARI, points: 10, rank: 9},
+  "ADENARI" : { suit: DENARI, points: 11, rank: 10},
+  "2SPADE" : { suit: SPADE, points: 0, rank: 1},
+  "4SPADE" : { suit: SPADE, points: 0, rank: 2},
+  "5SPADE" : { suit: SPADE, points: 0, rank: 3},
+  "6SPADE" : { suit: SPADE, points: 0, rank: 4},
+  "7SPADE" : { suit: SPADE, points: 0, rank: 5},
+  "JSPADE" : { suit: SPADE, points: 2, rank: 6},
+  "QSPADE" : { suit: SPADE, points: 3, rank: 7},
+  "KSPADE" : { suit: SPADE, points: 4, rank: 8},
+  "3SPADE" : { suit: SPADE, points: 10, rank: 9},
+  "ASPADE" : { suit: SPADE, points: 11, rank: 10},
+  "2COPPE" : { suit: COPPE, points: 0, rank: 1},
+  "4COPPE" : { suit: COPPE, points: 0, rank: 2},
+  "5COPPE" : { suit: COPPE, points: 0, rank: 3},
+  "6COPPE" : { suit: COPPE, points: 0, rank: 4},
+  "7COPPE" : { suit: COPPE, points: 0, rank: 5},
+  "JCOPPE" : { suit: COPPE, points: 2, rank: 6},
+  "QCOPPE" : { suit: COPPE, points: 3, rank: 7},
+  "KCOPPE" : { suit: COPPE, points: 4, rank: 8},
+  "3COPPE" : { suit: COPPE, points: 10, rank: 9},
+  "ACOPPE" : { suit: COPPE, points: 11, rank: 10},
+  "2BASTONI" : { suit: BASTONI, points: 0, rank: 1},
+  "4BASTONI" : { suit: BASTONI, points: 0, rank: 2},
+  "5BASTONI" : { suit: BASTONI, points: 0, rank: 3},
+  "6BASTONI" : { suit: BASTONI, points: 0, rank: 4},
+  "7BASTONI" : { suit: BASTONI, points: 0, rank: 5},
+  "JBASTONI" : { suit: BASTONI, points: 2, rank: 6},
+  "QBASTONI" : { suit: BASTONI, points: 3, rank: 7},
+  "KBASTONI" : { suit: BASTONI, points: 4, rank: 8},
+  "3BASTONI" : { suit: BASTONI, points: 10, rank: 9},
+  "ABASTONI" : { suit: BASTONI, points: 11, rank: 10}
+}
+
+
+
+
